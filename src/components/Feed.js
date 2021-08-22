c380d6641cb613570831cd0252a5a7ba69cad011
hugues0 <ntwari.hugues@gmail.com>
2021-08-22 11:03:52 +0200

    posts section

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,4 +1,5 @@
 import { Container, makeStyles } from "@material-ui/core";
+import Post from './Post'
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -8,9 +9,16 @@ const useStyles = makeStyles((theme) => ({
 
 const Feed = () => {
   const classes = useStyles();
-  return <Container className={classes.container}>
-    Rightbar
-  </Container>; 
+  return (
+    <Container className={classes.container}>
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </Container>
+  ); 
 };
 
 export default Feed;
---
+++ src/components/Post.js
@@ -0,0 +1,51 @@
+import {
+    Button,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Container,
+  makeStyles,
+  Typography,
+} from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  media:{
+      height:250
+  },
+  card:{
+      marginBottom:theme.spacing(3)
+  }
+}));
+
+const Post = () => {
+  const classes = useStyles();
+  return (
+    <Card className={classes.card}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="https://pbs.twimg.com/profile_images/1200529452/Bart_Simpson_400x400.jpg"
+          title="My Post"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5">My first Post</Typography>
+          <Typography variant="body2">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged.{" "}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">Share</Button>
+        <Button size="small" color="primary">Learn more</Button>
+      </CardActions>
+    </Card>
+  );
+};
+export default Post;
