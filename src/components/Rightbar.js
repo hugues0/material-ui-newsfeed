37b35f7839b8de9380fcbf11d750d928568251a9
hugues0 <ntwari.hugues@gmail.com>
2021-08-23 11:27:24 +0200

    project finito

--- src/components/Rightbar.js
+++ src/components/Rightbar.js
@@ -1,14 +1,30 @@
-import { Avatar, Container, ImageList, ImageListItem, makeStyles, Typography } from "@material-ui/core";
+import {
+  Avatar,
+  Container,
+  Divider,
+  ImageList,
+  ImageListItem,
+  Link,
+  makeStyles,
+  Typography,
+} from "@material-ui/core";
 import { AvatarGroup } from "@material-ui/lab";
 
 const useStyles = makeStyles((theme) => ({
   container: {
     paddingTop: theme.spacing(10),
+    position:"sticky",
+    top:0
   },
-  title:{
-    fontSize:16,
-    fontWeight:500,
-    color:"#555"
+  title: {
+    fontSize: 16,
+    fontWeight: 500,
+    color: "#555",
+  },
+  link:{
+    marginRight:theme.spacing(2),
+    color:"#555",
+    fontWeight:500
   }
 }));
 
@@ -19,7 +35,7 @@ const Rightbar = () => {
       <Typography className={classes.title} gutterBottom>
         Online Friends
       </Typography>
-      <AvatarGroup max={6}>
+      <AvatarGroup max={6} style={{ marginBottom: "20" }}>
         <Avatar
           alt="Remy Sharp"
           src="https://material-ui.com/static/images/avatar/1.jpg"
@@ -52,7 +68,12 @@ const Rightbar = () => {
       <Typography className={classes.title} gutterBottom>
         Gallery
       </Typography>
-      <ImageList rowHeight={160} className={classes.imageList} cols={3}>
+      <ImageList
+        rowHeight={100}
+        style={{ marginBottom:10}}
+        className={classes.imageList}
+        cols={2}
+      >
         <ImageListItem>
           <img
             src="https://material-ui.com/static/images/image-list/vegetables.jpg"
@@ -90,6 +111,22 @@ const Rightbar = () => {
           />
         </ImageListItem>
       </ImageList>
+      <Typography className={classes.title} gutterBottom>
+        Categories
+      </Typography>
+      <Link href="#" className={classes.link} variant="body2">
+        Sport
+      </Link>
+      <Link href="#" className={classes.link} variant="body2">
+        Food
+      </Link>
+      <Divider flexItem style={{marginBottom:5}}/>
+      <Link href="#" className={classes.link} variant="body2">
+        Movies
+      </Link>
+      <Link href="#" className={classes.link} variant="body2">
+        Science
+      </Link>
     </Container>
   );
 };
