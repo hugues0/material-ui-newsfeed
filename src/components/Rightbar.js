f9acd74d40345aaed66c58d6d9947bc40936894f
hugues0 <ntwari.hugues@gmail.com>
2021-08-23 10:31:09 +0200

    right bar updated

--- src/components/Rightbar.js
+++ src/components/Rightbar.js
@@ -1,14 +1,97 @@
-import { Container, makeStyles } from "@material-ui/core";
+import { Avatar, Container, ImageList, ImageListItem, makeStyles, Typography } from "@material-ui/core";
+import { AvatarGroup } from "@material-ui/lab";
 
 const useStyles = makeStyles((theme) => ({
   container: {
     paddingTop: theme.spacing(10),
   },
+  title:{
+    fontSize:16,
+    fontWeight:500,
+    color:"#555"
+  }
 }));
 
 const Rightbar = () => {
   const classes = useStyles();
-  return <Container className={classes.container}>Rightbar</Container>;
+  return (
+    <Container className={classes.container}>
+      <Typography className={classes.title} gutterBottom>
+        Online Friends
+      </Typography>
+      <AvatarGroup max={6}>
+        <Avatar
+          alt="Remy Sharp"
+          src="https://material-ui.com/static/images/avatar/1.jpg"
+        />
+        <Avatar
+          alt="Travis Howard"
+          src="https://material-ui.com/static/images/avatar/2.jpg"
+        />
+        <Avatar
+          alt="Cindy Baker"
+          src="https://material-ui.com/static/images/avatar/3.jpg"
+        />
+        <Avatar
+          alt="Agnes Walker"
+          src="https://material-ui.com/static/images/avatar/4.jpg"
+        />
+        <Avatar
+          alt="Trevor Henderson"
+          src="https://material-ui.com/static/images/avatar/5.jpg"
+        />
+        <Avatar
+          alt="Trevor Henderson"
+          src="https://material-ui.com/static/images/avatar/6.jpg"
+        />
+        <Avatar
+          alt="Trevor Henderson"
+          src="https://material-ui.com/static/images/avatar/7.jpg"
+        />
+      </AvatarGroup>
+      <Typography className={classes.title} gutterBottom>
+        Gallery
+      </Typography>
+      <ImageList rowHeight={160} className={classes.imageList} cols={3}>
+        <ImageListItem>
+          <img
+            src="https://material-ui.com/static/images/image-list/vegetables.jpg"
+            alt=""
+          />
+        </ImageListItem>
+        <ImageListItem>
+          <img
+            src="https://material-ui.com/static/images/image-list/star.jpg"
+            alt=""
+          />
+        </ImageListItem>
+        <ImageListItem>
+          <img
+            src="https://material-ui.com/static/images/image-list/mushroom.jpg"
+            alt=""
+          />
+        </ImageListItem>
+        <ImageListItem>
+          <img
+            src="https://material-ui.com/static/images/image-list/olive.jpg"
+            alt=""
+          />
+        </ImageListItem>
+        <ImageListItem>
+          <img
+            src="https://material-ui.com/static/images/image-list/honey.jpg"
+            alt=""
+          />
+        </ImageListItem>
+        <ImageListItem>
+          <img
+            src="https://material-ui.com/static/images/image-list/bike.jpg"
+            alt=""
+          />
+        </ImageListItem>
+      </ImageList>
+    </Container>
+  );
 };
 
 export default Rightbar;
